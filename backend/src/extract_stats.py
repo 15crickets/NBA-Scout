655dad29bfa8148577a41db5a8b3e475065a5574
sc30714 <vikram.vasudevan@comcast.net>
2026-05-14 15:44:06 -0700

    Some brief frontend work and adding all-time ranks

--- backend/src/extract_stats.py
+++ backend/src/extract_stats.py
@@ -1,8 +1,47 @@
 from nba_api.stats.endpoints import shotchartdetail
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import playerdashboardbyclutch
+from nba_api.stats.endpoints import alltimeleadersgrids
 
 import time
+
+
+
+def all_time_rankings(player_name):
+    player_dict = players.find_players_by_full_name(player_name)[0]
+
+    id = player_dict["id"]
+
+    leaders = alltimeleadersgrids.AllTimeLeadersGrids(
+        league_id="00",
+        per_mode_simple="Totals",
+        season_type="Regular Season",
+        topx="5000"
+    )
+
+    gp_df = leaders.get_data_frames()[0]
+    points_df = leaders.get_data_frames()[1]
+    assists_df = leaders.get_data_frames()[2]
+    steals_df = leaders.get_data_frames()[3]
+    rebounds_df = leaders.get_data_frames()[6]
+    blocks_df = leaders.get_data_frames()[7]
+
+
+    return {
+        "Games Played": gp_df.loc[gp_df['PLAYER_ID'] == id]["GP"].iloc[0],
+        "Games Played Rank": gp_df.loc[gp_df['PLAYER_ID'] == id]["GP_RANK"].iloc[0],
+        "Total Points": points_df.loc[points_df['PLAYER_ID'] == id]["PTS"].iloc[0],
+        "Total Points Rank": points_df.loc[points_df['PLAYER_ID'] == id]["PTS_RANK"].iloc[0],
+        "Total Assists": assists_df.loc[assists_df['PLAYER_ID'] == id]["AST"].iloc[0],
+        "Total Assists Rank": assists_df.loc[assists_df['PLAYER_ID'] == id]["AST_RANK"].iloc[0],
+        "Total Steals": steals_df.loc[steals_df['PLAYER_ID'] == id]["STL"].iloc[0],
+        "Total Steals Rank": steals_df.loc[steals_df['PLAYER_ID'] == id]["STL_RANK"].iloc[0],
+        "Total Rebounds": rebounds_df.loc[rebounds_df['PLAYER_ID'] == id]["REB"].iloc[0],
+        "Total Rebounds Rank": rebounds_df.loc[rebounds_df['PLAYER_ID'] == id]["REB_RANK"].iloc[0],
+        "Total Blocks": blocks_df.loc[blocks_df['PLAYER_ID'] == id]["BLK"].iloc[0],
+        "Total Blocks Rank": blocks_df.loc[blocks_df['PLAYER_ID'] == id]["BLK_RANK"].iloc[0],
+
+    }
 
 
 def get_id(player_name):
@@ -12,14 +51,8 @@ def get_id(player_name):
 
     return id
 
-def closest_defender():
-
-
-    return
-
 
 def get_clutch_stats(player_name):
-    time.sleep(1)
     id = get_id(player_name)
     shots = shotchartdetail.ShotChartDetail(
         team_id=0,
@@ -136,6 +169,8 @@ def get_clutch_stats(player_name):
 
 def get_team_stats(player_name):
 
+
+
     return
 
 def get_career_stats(player_name):
@@ -181,16 +216,82 @@ def get_zones(player_name):
     )
 
 
-    print(zone_splits["FG_PCT"])
+    atb_three_fgm = 0
+    atb_three_fga = 0
 
-    return zone_splits["FG_PCT"]
+    right_corner_three_fga = 0
+    right_corner_three_fgm = 0
+
+    left_corner_three_fga = 0
+    left_corner_three_fgm = 0
+
+    mid_range_fga = 0
+    mid_range_fgm = 0
+
+    atb_three_fga = 0
+    atb_three_fgm = 0
+
+    itp_fga = 0
+    itp_fgm = 0
+
+    ra_fga = 0
+    ra_fgm = 0
+    
+    if zone_splits.get("Above the Break 3", 0) != 0:
+        atb_three_fga = zone_splits.loc["Above the Break 3", "FGA"]
+        atb_three_fgm = zone_splits.loc["Above the Break 3", "FGM"]
+
+    if zone_splits.get("Mid-Range", 0) != 0:
+        mid_range_fga = zone_splits.get["Mid-Range", "FGA"]
+        mid_range_fgm = zone_splits.loc["Mid-Range", "FGM"]
+    
+    if zone_splits.get("Right Corner 3", 0) != 0:
+        right_corner_three_fga = zone_splits.get["Right Corner 3", "FGA"]
+        right_corner_three_fgm = zone_splits.get["Right Corner 3", "FGM"]
+
+    if zone_splits.get("Left Corner 3", 0) != 0:
+        left_corner_three_fga = zone_splits.get["Left Corner 3", "FGA"]
+        left_corner_three_fgm = zone_splits.get["Left Corner 3", "FGM"]
+
+    if zone_splits.get("In The Paint (Non-RA)", 0) != 0:
+        itp_fga = zone_splits.loc["In The Paint (Non-RA)", "FGA"]
+        itp_fgm = zone_splits.loc["In The Paint (Non-RA)", "FGM"]
+
+    if zone_splits.get("Restricted Area", 0) != 0:
+        ra_fga = zone_splits.loc["Restricted Area", "FGA"]
+        ra_fgm = zone_splits.loc["Restricted Area", "FGM"]
+
+
+    overall_fg_attempts = atb_three_fga + mid_range_fga + right_corner_three_fga + left_corner_three_fga + itp_fga + ra_fga
+    overall_fg_makes = atb_three_fgm + mid_range_fgm + right_corner_three_fgm + left_corner_three_fgm + itp_fgm + ra_fgm
+
+    fg_percentage = overall_fg_makes/overall_fg_attempts
+
+    overall_3pa = atb_three_fga + right_corner_three_fga + left_corner_three_fga
+    overall_3pm = atb_three_fgm + right_corner_three_fgm + left_corner_three_fgm
+
+    three_point_percentage = overall_3pm/overall_3pa
+
+    return {
+        "Mid-Range FGA": mid_range_fga,
+        "Mid-Range FGM": mid_range_fgm,
+        "Above the Break 3 FGA": atb_three_fga,
+        "Above the Break 3 FGM": atb_three_fgm,
+        "Right Corner 3 FGA": right_corner_three_fga,
+        "Right Corner 3 FGM": right_corner_three_fgm,
+        "Left Corner 3 FGA": left_corner_three_fga,
+        "Left Corner 3 FGM": left_corner_three_fgm,
+        "In The Paint (Non-RA) FGA": itp_fga,
+        "In The Paint (Non-RA) FGM": itp_fgm,
+        "Restricted Area FGA": ra_fga,
+        "Restricted Area FGM": ra_fgm,
+        "Overall FGA": overall_fg_attempts,
+        "Overall FGM": overall_fg_makes,
+        "Overall FG Percentage": fg_percentage,
+        "Overall 3PA": overall_3pa,
+        "Overall 3PM": overall_3pm,
+        "Overall 3 Point Percentage": three_point_percentage
+    }
     
 
-
-
-
-
-
-get_zones("Lebron James")
-
-get_clutch_stats("Lebron James")
+print(all_time_rankings("Lebron James"))
--- nba-scout/components/HotZones.tsx
+++ nba-scout/components/HotZones.tsx
@@ -1,128 +1,174 @@
 export default function HotZones() {
     return (
         <svg
-            viewBox="0 0 500 470"
-            className="w-full h-full bg-orange-100"
+            viewBox="0 0 600 470"
+            className="w-full h-full"
         >
-            {/* COURT OUTLINE */}
-            <rect
-                x="10"
-                y="10"
-                width="480"
-                height="450"
+            {/* Court background */}
+            <rect x="0" y="0" width="600" height="470" fill="#1a1a1a"/>
+            
+            {/* Court outline */}
+            <rect 
+                x="20" 
+                y="20" 
+                width="560" 
+                height="430" 
+                fill="none" 
+                stroke="#fff" 
+                strokeWidth="3"
+            />
+            
+            {/* Baseline */}
+            <line 
+                x1="20" 
+                y1="450" 
+                x2="580" 
+                y2="450" 
+                stroke="#fff" 
+                strokeWidth="3"
+            />
+            
+            {/* Three-point arc */}
+            <path 
+                d="M 60 450 Q 60 430, 70 410 A 245 245 0 0 1 530 410 Q 540 430, 540 450" 
+                stroke="#fff" 
+                strokeWidth="3" 
                 fill="none"
-                stroke="black"
+            />
+            
+            {/* Above the Break 3 - BLUE (outside 3pt line, above corners) */}
+            <path 
+                id="above-break-3"
+                d="M 70 410
+                   A 245 245 0 0 1 530 410
+                   L 530 20
+                   L 70 20
+                   Z" 
+                fill="#3b82f6" 
+                stroke="#fff" 
                 strokeWidth="2"
+                data-zone="above-break-3"
             />
-
-            {/* BASKET (top center) */}
-            <circle
-                cx="250"
-                cy="40"
-                r="7"
-                fill="orange"
-                stroke="black"
-            />
-
-            {/* PAINT (key) */}
-            <rect
-                x="170"
-                y="40"
-                width="160"
-                height="120"
-                fill="rgba(0,0,255,0.15)"
-                stroke="blue"
-                strokeWidth="1"
-            />
-
-            {/* FREE THROW LINE */}
-            <line
-                x1="170"
-                y1="160"
-                x2="330"
-                y2="160"
-                stroke="black"
+            
+            {/* Left Corner 3 - GREEN */}
+            <path 
+                id="left-corner-3"
+                d="M 20 450 L 60 450 Q 60 430, 70 410 L 70 240 L 20 240 Z" 
+                fill="#22c55e" 
+                stroke="#fff" 
                 strokeWidth="2"
+                data-zone="left-corner-3"
             />
-
-            {/* TOP OF KEY ZONE (arc area simplified as polygon) */}
-            <polygon
-                points="
-                    170,160
-                    330,160
-                    370,260
-                    130,260
-                "
-                fill="rgba(255,0,0,0.12)"
-                stroke="red"
-                strokeWidth="1"
+            
+            {/* Right Corner 3 - GREEN */}
+            <path 
+                id="right-corner-3"
+                d="M 580 450 L 540 450 Q 540 430, 530 410 L 530 240 L 580 240 Z" 
+                fill="#22c55e" 
+                stroke="#fff" 
+                strokeWidth="2"
+                data-zone="right-corner-3"
             />
-
-            {/* LEFT CORNER 3 */}
-            <polygon
-                points="
-                    10,260
-                    130,260
-                    130,400
-                    10,400
-                "
-                fill="rgba(0,255,0,0.12)"
-                stroke="green"
-                strokeWidth="1"
+            
+            {/* Midrange - ORANGE (extended oval inside 3pt, outside paint) */}
+            <path 
+                id="midrange"
+                d="M 70 410
+                   A 245 245 0 0 1 530 410
+                   L 530 240
+                   A 230 190 0 0 0 70 240
+                   Z
+                   M 210 252
+                   A 90 90 0 0 1 390 252
+                   L 390 450
+                   L 210 450
+                   L 210 252
+                   Z" 
+                fill="#f97316" 
+                stroke="#fff" 
+                strokeWidth="2"
+                data-zone="midrange"
             />
-
-            {/* RIGHT CORNER 3 */}
-            <polygon
-                points="
-                    370,260
-                    490,260
-                    490,400
-                    370,400
-                "
-                fill="rgba(0,255,0,0.12)"
-                stroke="green"
-                strokeWidth="1"
+            
+            {/* Paint outline */}
+            <rect 
+                x="210" 
+                y="252" 
+                width="180" 
+                height="198" 
+                fill="none" 
+                stroke="#fff" 
+                strokeWidth="3"
             />
-
-            {/* LEFT WING / MIDRANGE */}
-            <polygon
-                points="
-                    130,260
-                    170,160
-                    170,400
-                    130,400
-                "
-                fill="rgba(255,165,0,0.12)"
-                stroke="orange"
-                strokeWidth="1"
+            
+            {/* Free throw circle top arc */}
+            <path 
+                d="M 210 252 A 90 90 0 0 1 390 252" 
+                stroke="#fff" 
+                strokeWidth="3" 
+                fill="none"
             />
-
-            {/* RIGHT WING / MIDRANGE */}
-            <polygon
-                points="
-                    330,160
-                    370,260
-                    370,400
-                    330,400
-                "
-                fill="rgba(255,165,0,0.12)"
-                stroke="orange"
-                strokeWidth="1"
+            
+            {/* Free throw circle bottom arc (dashed) */}
+            <path 
+                d="M 210 252 A 90 90 0 0 0 390 252" 
+                stroke="#fff" 
+                strokeWidth="3" 
+                strokeDasharray="5,5"
+                fill="none"
             />
-
-            {/* CENTER STRAIGHT ZONE */}
-            <polygon
-                points="
-                    170,160
-                    330,160
-                    370,260
-                    330,400
-                    170,400
-                    130,260
-                "
-                fill="rgba(128,0,128,0.08)"
-                stroke="purple"
-                strokeWidth="1"
+            
+            {/* Paint Non-Restricted Area - PURPLE */}
+            <path 
+                id="paint-non-restricted"
+                d="M 210 252 
+                   L 210 450
+                   L 390 450
+                   L 390 252
+                   A 90 90 0 0 0 210 252
+                   M 260 438
+                   A 40 40 0 0 0 340 438
+                   L 340 450
+                   L 260 450
+                   Z" 
+                fill="#a855f7" 
+                stroke="#fff" 
+                strokeWidth="2"
+                data-zone="paint-non-restricted"
+            />
+            
+            {/* Restricted Area semicircle - RED */}
+            <path 
+                id="restricted-area"
+                d="M 260 438 
+                   A 40 40 0 0 0 340 438
+                   L 340 450
+                   L 260 450
+                   Z" 
+                fill="#ef4444" 
+                stroke="#fff" 
+                strokeWidth="3"
+                data-zone="restricted-area"
+            />
+            
+            {/* Basket */}
+            <circle 
+                cx="300" 
+                cy="438" 
+                r="9" 
+                fill="none" 
+                stroke="#ff6b35" 
+                strokeWidth="3"
+            />
+            
+            {/* Backboard */}
+            <line 
+                x1="270" 
+                y1="450" 
+                x2="330" 
+                y2="450" 
+                stroke="#fff" 
+                strokeWidth="4"
             />
         </svg>
     )
